af8cc98fc4bb17c363668c87b73bedc58b717674
Hassan-Ahamad172636 <hassanahmad172636@gmail.com>
2025-12-03 07:59:07 -0800

    latest code

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 import express from "express";
 import cors from "cors";
-import UserRoutes from "../src/routes/user.route.js"
-import ConversationRoutes from "../src/routes/conversation.route.js"
+import UserRoutes from "../routes/user.route.js"
+import ConversationRoutes from "../routes/conversation.route.js"
 import mongoose from "mongoose";
+import connectDB from "./database/server.js";  // <- FIX
 import { DB_NAME } from "./constant.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// ✨ FIX: call the database connection BEFORE routes
+connectDB();
 
 // TEST ROUTE
 app.get("/", (req, res) => {
@@ -18,12 +22,11 @@ app.get("/debug", (req, res) => {
   res.send({
     uri: process.env.MONGO_DB_URI,
     name: DB_NAME,
-    connected: mongoose.connection.readyState
+    connected: mongoose.connection.readyState  // 1 = connected
   });
 });
 
-
-app.use('/user', UserRoutes)
-app.use('/conversation', ConversationRoutes)
+app.use("/user", UserRoutes);
+app.use("/conversation", ConversationRoutes);
 
 export default app;
